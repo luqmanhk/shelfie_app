f20a0431adf1088413340eab416292f103125733
luqmanhk <luqman.kabiru@gmail.com>
2026-01-29 21:06:50 -0500

    updates to home page with logo

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+import Logo from '../assets/shelfie.png'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style = {styles.container}>
+        <Image source = {Logo} style = {styles.img} />
+        
+      <Text style = {styles.title}>The Number 1</Text>
+      
+      <Text style = {{ marginTop: 10, marginBottom: 30 }}>
+        Reading List App
+        </Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    img: {
+        marginVertical: 20,    
+        width: 100,
+        height: 100
+    },
+})
