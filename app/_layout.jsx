0b6129cde293929469ae32ab202b30a5e89d6344
luqmanhk <luqman.kabiru@gmail.com>
2026-02-01 22:22:10 -0500

    tuning out light and dark mode

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,21 +2,24 @@ import { StyleSheet, Text, useColorScheme, View } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
 import { Colors } from "../constants/colors"
+import { StatusBar } from 'expo-status-bar'
 
 const RootLayout = () => {
   const colorScheme = useColorScheme()
   const theme = Colors[colorScheme] ?? Colors.light
 
   return (
+    <>
+    < StatusBar value = "auto" />
     < Stack screenOptions = {{
-      headerStyle: { backgroundColor: '#ddd' },
-      headerTintColor: "#333"
+      headerStyle: { backgroundColor: theme.navBackground },
+      headerTintColor: theme.title,
     }}> 
       <Stack.Screen name = "index" options = {{ title: 'Home'}}/>
       <Stack.Screen name = "about" options = {{ title: 'About Us'}}/>
-      <Stack.Screen name = "contact" options = {{ title: 'Contact Us', headerShown: false }}/>
+      <Stack.Screen name = "contact" options = {{ title: 'Contact Us' }}/>
     </Stack>
-
+    </>
   )
 }
 
